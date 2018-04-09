15586a30259924176c094d1e666b69cec7fc5eaf
Miggi <miras.mustimov@tuta.io>
2018-04-09 23:48:46 +0600

    change bg on rest item select

---
+++ resources/assets/js/components/RestCenters/ListItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div @click="$emit('selected')" class="p-4 border-b border-grey-light" :class="{ 'bg-grey-lighter': active }">
+        <div class="mb-3">
+            <h3 class="text-base text-black font-semibold">{{ restCenter.name }}</h3>
+        </div>
+
+        <div class="text-base text-grey-dark">
+            {{ restCenter.reservoir.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [ 'restCenter', 'active' ]
+    }
+</script>
