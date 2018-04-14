635878d5a0917ee5b7d7753e242303f6255e98d6
Miggi <miras.mustimov@tuta.io>
2018-04-14 13:57:49 +0600

    add word rap to reservoirs in search panel

--- resources/assets/js/components/RestCenters/SearchPanel.vue
+++ resources/assets/js/components/RestCenters/SearchPanel.vue
@@ -5,7 +5,7 @@
         </div>
 
         <!-- Reservoirs -->
-        <div class="whitespace-no-wrap">
+        <div>
             <div class="flex mb-1 px-2">
                 <h4 class="text-lg text-black font-semibold">Водоем</h4>
             </div>
