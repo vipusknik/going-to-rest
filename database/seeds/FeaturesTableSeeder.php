64a60380e456cd003579ce82c76cb2bd6c3af27c
Miggi <miras.mustimov@tuta.io>
2018-04-15 22:34:09 +0600

    Refactor features table seeder

--- database/seeds/FeaturesTableSeeder.php
+++ database/seeds/FeaturesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Feature;
 
 class FeaturesTableSeeder extends Seeder
 {
@@ -12,112 +13,65 @@ class FeaturesTableSeeder extends Seeder
     public function run()
     {
         $accomodationFacilities = [
-            [
-                'name' => 'Туалет',
-            ],
-
-            [
-                'name' => 'Вода',
-            ],
-
-            [
-                'name' => 'Плита',
-            ],
-
-            [
-                'name' => 'Холодильник',
-            ],
-
-            [
-                'name' => 'Постельное белье',
-            ],
-
-
-            [
-                'name' => 'Душ',
-            ],
-
-            [
-                'name' => 'Телевизор',
-            ],
-
-            [
-                'name' => 'Посуда',
-            ],
+            [ 'name' => 'Туалет' ],
+            [ 'name' => 'Вода' ],
+            [ 'name' => 'Плита' ],
+            [ 'name' => 'Холодильник' ],
+            [ 'name' => 'Постельное белье' ],
+            [ 'name' => 'Душ' ],
+            [ 'name' => 'Телевизор' ],
+            [ 'name' => 'Посуда' ],
         ];
 
         foreach ($accomodationFacilities as &$feature) {
-            $feature['belongs_to'] = 'accomodation';
-            $feature['category'] = 'facilities';
+            $feature['belongs_to'] = Feature::OF_ACCOMODATION;
+            $feature['category'] = Feature::CATEGORY_FACILITIES;
         }
 
         DB::table('features')->insert($accomodationFacilities);
 
         $restCenterFacilities = [
-            [
-                'name' => 'Баня/сауна',
-            ],
-
-            [
-                'name' => 'Мангал',
-            ],
-
-            [
-                'name' => 'Магазин',
-            ],
-
-            [
-                'name' => 'Кафе',
-            ],
-
-            [
-                'name' => 'Спорт площадки',
-            ],
-
-
-            [
-                'name' => 'Прокат снаряжения',
-            ],
-
-            [
-                'name' => 'Прокат техники',
-            ],
+            [ 'name' => 'Баня/сауна' ],
+            [ 'name' => 'Мангал' ],
+            [ 'name' => 'Магазин' ],
+            [ 'name' => 'Кафе' ],
+            [ 'name' => 'Спорт площадки' ],
+            [ 'name' => 'Прокат снаряжения' ],
+            [ 'name' => 'Прокат техники' ],
         ];
 
         foreach ($restCenterFacilities as &$feature) {
-            $feature['belongs_to'] = 'rest_center';
-            $feature['category'] = 'facilities';
+            $feature['belongs_to'] = Feature::OF_REST_CENTER;
+            $feature['category'] = Feature::CATEGORY_FACILITIES;
         }
 
         DB::table('features')->insert($restCenterFacilities);
 
         $restCenterLeasures = [
-            [
-                'name' => 'Рыбалка',
-            ],
-
-            [
-                'name' => 'Охота',
-            ],
-
-            [
-                'name' => 'Купание',
-            ],
-
-            [
-                'name' => 'Сбор грибов',
-            ],
-
-            [
-                'name' => 'Пешая прогулка',
-            ],
+            [ 'name' => 'Рыбалка' ],
+            [ 'name' => 'Охота' ],
+            [ 'name' => 'Купание' ],
+            [ 'name' => 'Сбор грибов' ],
+            [ 'name' => 'Пешая прогулка' ],
         ];
 
         foreach ($restCenterLeasures as &$feature) {
-            $feature['belongs_to'] = 'rest_center';
-            $feature['category'] = 'leasures';
+            $feature['belongs_to'] = Feature::OF_REST_CENTER;
+            $feature['category'] = Feature::CATEGORY_LEASURES;
         }
 
         DB::table('features')->insert($restCenterLeasures);
+
+        $medicalCenterTreatmentTypes = [
+            [  'name' => 'Пантолечение' ],
+            [  'name' => 'Грязевые ванны' ],
+        ];
+
+        foreach ($medicalCenterTreatmentTypes as &$feature) {
+            $feature['belongs_to'] = Feature::OF_MEDICAL_CENTER;
+            $feature['category'] = Feature::CATEGORY_TREATMENT_TYPES;
+        }
+
+        DB::table('features')->insert($medicalCenterTreatmentTypes);
     }
 }
