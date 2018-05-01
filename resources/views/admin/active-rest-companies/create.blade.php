1f1d557ce10f11b00c532a71824244445b2867b2
Miggi <miras.mustimov@tuta.io>
2018-05-01 16:37:49 +0600

    Extracted contact inputs from all forms

--- resources/views/admin/active-rest-companies/create.blade.php
+++ resources/views/admin/active-rest-companies/create.blade.php
@@ -60,58 +60,7 @@
                             </div>
                         </div>
 
-                        <div class="field-body flex mb-4">
-                          <div class="field w-1/3 mr-3">
-                            <label class="label">
-                              Телефоны <span class="text-sm text-grey-dark">(укажите через запятую)</span>
-                            </label>
-
-                            <div class="control">
-                              <input class="input {{ $errors->has('contacts') ? ' is-danger' : '' }}"
-                                     type="text"
-                                     name="contacts"
-                                     value="{{ old('contacts') }}"
-                                     placeholder="Телефоны"
-                                     required="true">
-                            </div>
-
-                            @if ($errors->has('contacts'))
-                              <p class="help is-danger">{{ $errors->first('contacts') }}</p>
-                            @endif
-                          </div>
-
-                          <div class="field w-1/3 mr-3">
-                            <label class="label">Email</label>
-
-                            <div class="control">
-                              <input class="input {{ $errors->has('email') ? ' is-danger' : '' }}"
-                                     type="email"
-                                     name="email"
-                                     value="{{ old('email') }}"
-                                     placeholder="Email">
-                            </div>
-
-                            @if ($errors->has('email'))
-                              <p class="help is-danger">{{ $errors->first('email') }}</p>
-                            @endif
-                          </div>
-
-                          <div class="field w-1/3">
-                            <label class="label">Ссылка на сайт</label>
-
-                            <div class="control">
-                              <input class="input"
-                                     type="text"
-                                     name="site_link"
-                                     value="{{ old('site_link') }}"
-                                     placeholder="Ссылка на сайт">
-                            </div>
-                          </div>
-                        </div>
-
-                        <div class="mb-6">
-                            @include ('admin.social-media.inputs', [ 'socialMedia' => null ])
-                        </div>
+                        @include('admin.partials.contact-inputs', [ 'model' => null ])
 
                         <div class="mb-6">
                           @if ($errors->has('activities'))
--- resources/views/admin/medical-centers/edit.blade.php
+++ resources/views/admin/medical-centers/edit.blade.php
@@ -61,57 +61,7 @@
                             </div>
                         </div>
 
-                        <div class="field-body flex mb-4">
-                          <div class="field w-1/3 mr-3">
-                            <label class="label">
-                              Телефоны <span class="text-sm text-grey-dark">(укажите через запятую)</span>
-                            </label>
-
-                            <div class="control">
-                              <input class="input {{ $errors->has('contacts') ? ' is-danger' : '' }}"
-                                     type="text"
-                                     name="contacts"
-                                     value="{{ old('contacts', $medicalCenter->contacts) }}"
-                                     placeholder="Телефоны">
-                            </div>
-
-                            @if ($errors->has('contacts'))
-                              <p class="help is-danger">{{ $errors->first('contacts') }}</p>
-                            @endif
-                          </div>
-
-                          <div class="field w-1/3 mr-3">
-                            <label class="label">Email</label>
-
-                            <div class="control">
-                              <input class="input {{ $errors->has('email') ? ' is-danger' : '' }}"
-                                     type="email"
-                                     name="email"
-                                     value="{{ old('email', $medicalCenter->email) }}"
-                                     placeholder="Email">
-                            </div>
-
-                            @if ($errors->has('email'))
-                              <p class="help is-danger">{{ $errors->first('email') }}</p>
-                            @endif
-                          </div>
-
-                          <div class="field w-1/3">
-                            <label class="label">Ссылка на сайт</label>
-
-                            <div class="control">
-                              <input class="input"
-                                     type="text"
-                                     name="site_link"
-                                     value="{{ old('site_link', $medicalCenter->site_link) }}"
-                                     placeholder="Ссылка на сайт">
-                            </div>
-                          </div>
-                        </div>
-
-                        <div class="mb-6">
-                            @include ('admin.social-media.inputs', [ 'socialMedia' => $medicalCenter->social_media_sites ])
-                        </div>
+                        @include('admin.partials.contact-inputs', [ 'model' => $medicalCenter ])
 
                         <features-attach :features="{{ json_encode($features) }}"
                                          :features-attached="{{ json_encode($medicalCenter->features) }}"
