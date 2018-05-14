3428306dac9a024760863869ee3c67ff1f884675
Miggi <miras.mustimov@tuta.io>
2018-05-14 23:48:21 +0600

    style rest centers

--- resources/assets/js/components/UI/RestCenters/RestCenter.vue
+++ resources/assets/js/components/UI/RestCenters/RestCenter.vue
@@ -12,42 +12,42 @@
             <div class="flex flex-col md:flex-row">
                 <!-- List item image -->
                 <div class="md:self-end md:w-2/5">
-                    <img :src="previewImageUrl" alt="" class="block w-full h-auto md:rounded-bl-2xl md:rounded-tr-2xl">
+                    <img :src="previewImageUrl" alt="" class="block w-full h-48 md:rounded-bl-2xl md:rounded-tr-2xl" style="object-fit: cover; object-position: top">
                 </div>
 
                 <div class="pt-3 px-4 pb-1 md:w-3/5 md:flex md:flex-wrap md:relative">
                     <div class="flex items-center border-b border-dotted border-teal-dark py-2 md:w-1/2 md:border-b-2 md:border-r-2 md:pb-3 md:pr-1 md:items-start">
-                        <div class="w-1/4">
+                        <div class="mr-3 md:w-1/4 md:mr-1">
                             <img src="/images/icons/location.png" alt="address" class="block w-8 h-8">
                         </div>
 
-                        <div class="w-3/4">
+                        <div class="w-4/5 md:w-3/4">
                             <strong>{{ center.reservoir.name }}. </strong>{{ center.location }}
                         </div>
                     </div>
 
                     <div class="flex items-center border-b border-dotted border-teal-dark py-2 md:w-1/2 md:border-b-2 md:pb-3 md:pl-3 md:items-start">
-                        <div class="w-1/4">
+                        <div class="mr-3 md:w-1/4 md:mr-1">
                             <img src="/images/icons/contacts.png" alt="address" class="block w-8 h-8">
                         </div>
 
-                        <div class="w-3/4">
+                        <div class="w-4/5 md:w-3/4">
                             8 777 279 40 33, 8 777 279 40 33
                         </div>
                     </div>
 
                     <div class="flex items-center border-b border-dotted border-teal-dark py-2 md:w-1/2 md:border-b-0 md:border-r-2 md:pr-2 md:items-start">
-                        <div class="w-1/4">
+                        <div class="mr-3 md:w-1/4 md:mr-1">
                             <img src="/images/icons/price.png" alt="address" class="block w-8 h-8">
                         </div>
 
-                        <div class="w-3/4">
+                        <div class="w-4/5 md:w-3/4">
                             стоимость проживания от {{ chepestAccomodationPrice }} тг <span v-if="hasFood" class="text-red-light">возможно питание</span>
                         </div>
                     </div>
 
                     <div class="flex items-center border-teal-dark py-2 md:w-1/2 md:border-b-0 md:border-dotted md:border-teal-dark md:pl-3 md:items-start">
-                        <div class="w-1/4">
+                        <div class="mr-3 md:w-1/4 md:mr-1">
                             <img src="/images/icons/accomodation.png" alt="address" class="block w-8 h-8">
                         </div>
 
@@ -84,7 +84,7 @@
                         <img src="/images/icons/location.png" alt="address" class="block w-8 h-8">
                     </div>
 
-                    <div class="w-3/4">
+                    <div class="w-4/5 md:w-3/4">
                         <strong>{{ center.reservoir.name }}. </strong>{{ center.location }}
                     </div>
                 </div>
@@ -94,7 +94,7 @@
                         <img src="/images/icons/contacts.png" alt="address" class="block w-8 h-8">
                     </div>
 
-                    <div class="w-3/4">
+                    <div class="w-4/5 md:w-3/4">
                         {{ center.contacts }}
                     </div>
                 </div>
@@ -104,7 +104,7 @@
                         <img src="/images/icons/price.png" alt="address" class="block w-8 h-8">
                     </div>
 
-                    <div class="w-3/4">
+                    <div class="w-4/5 md:w-3/4">
                         стоимость проживания от  {{ chepestAccomodationPrice }} тг <span v-if="hasFood" class="text-red-light">возможно питание</span>
                     </div>
                 </div>
