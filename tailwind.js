3001efed7052af5e67f8b687ba10ac823a28f1f6
Miggi <miras.mustimov@tuta.io>
2018-05-14 22:48:30 +0600

    wow this yellow dark fits much better!

--- tailwind.js
+++ tailwind.js
@@ -74,7 +74,7 @@ let colors = {
 
   'yellow-darkest': '#453411',
   'yellow-darker': '#684f1d',
-  'yellow-dark': '#f2d024',
+  'yellow-dark': '#ebc54a',
   'yellow': '#ffed4a',
   'yellow-light': '#fff382',
   'yellow-lighter': '#fff9c2',
