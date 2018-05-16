6fc2ad9a83b50bfdb8240eac9fc9a460999ea45b
Miggi <miras.mustimov@tuta.io>
2018-05-16 23:17:40 +0600

    fix typo

--- app/Http/Requests/MedicalCentersRequest.php
+++ app/Http/Requests/MedicalCentersRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\validation\Rule;
+use Illuminate\Validation\Rule;
 
 class MedicalCentersRequest extends FormRequest
 {
