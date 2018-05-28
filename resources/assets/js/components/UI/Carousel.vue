b73933e86cd94fe85729c79afa1dfcf00600d38c
Miggi <miras.mustimov@tuta.io>
2018-05-28 23:31:09 +0600

    add carousel full screen support

--- resources/assets/js/components/UI/Carousel.vue
+++ resources/assets/js/components/UI/Carousel.vue
@@ -10,8 +10,38 @@
     </div>
 </template>
 
+<script>
+    import Flickity from 'flickity';
+    import 'flickity-as-nav-for';
+    import 'flickity-fullscreen';
+    import 'flickity/dist/flickity.min.css';
+    import 'flickity-fullscreen/fullscreen.css';
+    export default {
+        props: {
+            wrapAround: { default: true },
+            autoPlay: { default: false }
+        },
+        mounted() {
+            new Flickity('.main-carousel', {
+                autoPlay: this.autoPlay,
+                cellAlign: 'left',
+                contain: true,
+                pageDots: false,
+                fullscreen: true
+            });
+
+            new Flickity( '.carousel-nav', {
+              asNavFor: '.main-carousel',
+              pageDots: false,
+              cellAlign: 'left',
+              contain: true,
+            });
+        }
+    }
+</script>
+
 <style>
-    .main-carousel .flickity-button {
+    .main-carousel .flickity-prev-next-button {
         color: white !important;
         background: none !important;
         top: 85%;
@@ -19,6 +49,13 @@
 
     .main-carousel .flickity-button-icon {
         fill: currentColor;
+    }
+
+    .main-carousel .carousel-cell {
+        margin-right: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 
     @media (min-width: 768px) {
@@ -30,37 +67,21 @@
         }
     }
 
+    .is-fullscreen {
+        z-index: 60 !important;
+    }
+
+    .is-fullscreen .carousel-cell {
+        width: 100%;
+        height: 100%;
+        border-bottom-left-radius: 0 !important;
+        border-top-right-radius: 0 !important;
+        object-fit: contain;
+        object-position: initial;
+    }
+
     .carousel-nav img {
         width: 100px;
         height: 70px;
     }
 </style>
-
-<script>
-import Flickity from 'flickity';
-import 'flickity-as-nav-for';
-import 'flickity-fullscreen';
-import 'flickity/dist/flickity.min.css';
-export default {
-    props: {
-        wrapAround: { default: true },
-        autoPlay: { default: false }
-    },
-    mounted() {
-        new Flickity('.main-carousel', {
-            autoPlay: this.autoPlay,
-            cellAlign: 'left',
-            contain: true,
-            pageDots: false,
-            fullscreen: true
-        });
-
-        new Flickity( '.carousel-nav', {
-          asNavFor: '.main-carousel',
-          pageDots: false,
-          cellAlign: 'left',
-          contain: true,
-        });
-    }
-};
-</script>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
     <link rel="icon" type="image/png" href="/images/icons/walker.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>
-        @yield('title')
+        @yield('title') | {{ config('app.name') }}
     </title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
