f8a220232e21a311489029bf52ba9dbe9a38f5e9
Miggi <miras.mustimov@tuta.io>
2018-06-21 00:16:24 +0600

    +++

--- resources/assets/js/components/UI/JumpUpButton.vue
+++ resources/assets/js/components/UI/JumpUpButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div @click="onClick" class="hidden lg:flex lg:flex-col lg:items-center lg:z-40 lg:fixed lg:pin-b lg:pin-l lg:mb-12 lg:ml-12 lg:cursor-pointer">
-        <div class="bg-yellow-dark mb-1 border border-white">
+        <div class="bg-yellow-dark mb-1 border border-dashed border-white">
             <img src="/images/icons/up.png" class="block w-8 h-8">
         </div>
         <div class="text-xs text-white">Наверх</div>
